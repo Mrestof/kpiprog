84eafe2aa40745237a1f3134617b39fc175ff0eb
Mrestof <fotserm@gmail.com>
2021-11-19 22:48:20 +0200

    huffman:Add a few comments and minor changes

--- huffman/main.c
+++ huffman/main.c
@@ -3,13 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define BLOCKSIZE 512
+#define BLOCKSIZE 512 // size of the buffer block
 typedef struct _node {
   struct _node *left, *right;
   unsigned long freq;
   unsigned char c;
 } node;
-unsigned long freq[256];
+unsigned long freq[256]; // arr of frequencies, index corresponds to char code
 node *tree;
 
 int calc_freq(const char *input);
@@ -20,60 +20,72 @@ int main(int argc, char **argv) {
   int res;
   if (argc < 3) {
     printf("usage:  %s  <input_file>  <output_file>\n", argv[0]);
-    return -1;
+    return 1;
   }
   res = calc_freq(argv[1]);
   if (res != 0) {
     printf("Error  in  calc_freq!  %s\n", strerror(errno));
-    return -1;
+    return 1;
   }
-  for (int i = 0; i < 256; ++i) {
-    printf("%d:  %u\n", i, freq[i]);
+  for (int i = 0; i < 256; ++i) { // print out the entire `freq` array
+    printf("%d:  %lu\n", i, freq[i]);
   }
   return 0;
 }
 
 int calc_freq(const char *input) {
+  /* Calculate frequency of each character in char array and write it to the
+   * `freq` array.
+   */
   FILE *inp;
   int n;
   unsigned char buff[BLOCKSIZE];
-  if ((inp = fopen(input, "rb")) == NULL) {
-    return -1;
+
+  // by default pointer is pointing to NULL
+  if ((inp = fopen(input, "rb")) == NULL) { // exit if file can't be opened
+    printf("%p\n", (void *)inp);
+    printf("%p\n", (void *)&inp);
+    return 1;
   }
+
   do {
     n = fread(buff, BLOCKSIZE, 1, inp);
     for (int i = 0; i < n; ++i) {
       freq[buff[i]]++;
     }
   } while (n);
+
   return 0;
 }
 
 int build_tree() {
-  unsigned char indecies[256];
-  int i;
-  int k = 1;
-  node *leaves[256], *tmp;
+  unsigned char indices[256]; // array of corresponding indices for `freq` array
+  int i, k = 1, r = 0, l = 0;
+  node *leaves[1024], *tmp;
 
-  for (i = 0; i < 256; i++)
-    indecies[i] = i;
+  for (i = 0; i < 256; i++) {
+    indices[i] = i;
+  }
 
-  while (k > 0) {
+  while (k > 0) { // sort `indices` arr
     k = 0;
     for (i = 0; i < 255; i++) {
-      if (freq[indecies[i]] > freq[indecies[i + 1]]) {
-        indecies[i] = indecies[i] ^ indecies[i + 1];
-        indecies[i + 1] = indecies[i] ^ indecies[i + 1];
-        indecies[i] = indecies[i] ^ indecies[i + 1];
+      if (freq[indices[i]] > freq[indices[i + 1]]) {
+        indices[i] = indices[i] ^ indices[i + 1];
+        indices[i + 1] = indices[i] ^ indices[i + 1];
+        indices[i] = indices[i] ^ indices[i + 1];
         k++;
       }
     }
   }
 
   for (i = 0; i < 256; i++) {
-    leaves[indecies[i]] = (node *)malloc(sizeof(node));
-    leaves[indecies[i]]->c = i;
-    leaves[indecies[i]]->freq = freq[indecies[i]];
+    if (freq[indices[i]] == 0)
+      continue;
+    r++;
+    leaves[indices[i]] = (node *)malloc(sizeof(node));
+    leaves[indices[i]]->c = i;
+    leaves[indices[i]]->freq = freq[indices[i]];
   }
 
   return 0;
